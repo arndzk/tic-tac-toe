5d91d3f06ddcb61bffa44250a17c694a6a56df72
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-10-24 13:12:04 +0300

    progress: developed game tile factory, gameboard module + tile marks are updated in the gameboard object once tiles are clicked

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-function generateTiles() {
+// Player Factory
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+
+    return { name, mark, getName, getMark };
+};
+
+// Tile object Factory
+const tileFactory = (tileNum) => {
+    const tileMark = ``;
+    return { tileNum, tileMark };
+}
+
+// Create the gameboard object
+const gameBoardModule = (() => {
+    const generateGameTiles = () => {
+        const gameTiles = [];
+        for (let i = 0; i < 9; i++) {
+            gameTiles.push(tileFactory(i + 1));
+        }
+        return gameTiles;
+    }
+    const setTileMark = (tileNum, tileMark) => {
+        for (let i = 0; i < 9; i++) {
+            if (gameTiles[i].tileNum == tileNum) {
+                gameTiles[i].tileMark = tileMark;
+            }
+        }
+    }
+    const gameTiles = generateGameTiles();
+    const getGameBoard = () => console.log(gameTiles);
+    return { getGameBoard, setTileMark };
+})();
+
+
+// Generate the gameboard tiles in the DOM
+const dynamicDomTilesModule = (() => {
     let tiles = document.getElementById('tiles');
     for (let i = 0; i < 9; i++) {
         let tile = document.createElement('div');
@@ -7,23 +44,27 @@ function generateTiles() {
         tile.addEventListener('click', markTile);
         tiles.appendChild(tile);
     }
-}
+})();
 
 function markTile() {
     let whichTile = this.getAttribute('id');
-    console.log(`${whichTile} clicked!`);
 
-    let ticTac = document.createElement('span');
+    let ticTac = document.createElement('div');
     ticTac.classList.add('tic-tac-style');
     ticTac.innerHTML = `x`;
 
-    let toe = document.createElement('span');
+    let toe = document.createElement('div');
     toe.classList.add('toe-style');
     toe.innerHTML = `o`;
 
-    this.appendChild(toe);
+    this.appendChild(ticTac);
 
     this.removeEventListener('click', markTile);
+
+    updateTileMark(whichTile, ticTac.innerHTML);
 }
 
-generateTiles();
+const updateTileMark = (tileId, tileMark) => {
+    const tileNum = tileId.slice(-1);
+    gameBoardModule.setTileMark(tileNum, tileMark);
+}
